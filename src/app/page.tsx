96813de2644605d825a6c2f008bd5d5c2ca81bf5
UmeshKumar0143 <umeshkumar153654@gmail.com>
2025-07-26 13:19:55 +0530

    added next themes dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Home </div>
+    <div>
+      Home Page
+       </div>
+
   );
 }
